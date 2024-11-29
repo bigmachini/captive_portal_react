817cdbe9025084952ef0d7521c3df514a4c5f8d0
bigmachini <elisha.wambiji@bigmachini.net>
2024-11-29 19:25:40 +0300

    add server

--- server.js
+++ server.js
@@ -19,9 +19,13 @@ app.use(express.static(path.join(__dirname, 'build')));
 // Endpoint to handle form data
 app.post('/handle-form', (req, res) => {
 
-  const { mac, ip, username, link_login, link_login_only, link_orig, error } = req.body;
-  const queryParams = new URLSearchParams({ mac, ip, username, link_login, link_login_only, link_orig, error }).toString();
-  console.log(`Server is calling handle-form with ${queryParams}`);
+    const { mac, ip, username, error } = req.body;
+    const link_login = req.body['link-login'];
+    const link_login_only = req.body['link-login-only'];
+    const link_orig = req.body['link-orig'];
+  
+    const queryParams = new URLSearchParams({ mac, ip, username, link_login, link_login_only, link_orig, error }).toString();
+    console.log(`Server is calling handle-form with ${queryParams}`);
 
   res.redirect(`/?${queryParams}`);
 });
