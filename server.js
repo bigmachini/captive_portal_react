403fc02b8de107e8591dbcc35384eea73663bf23
bigmachini <elisha.wambiji@bigmachini.net>
2024-11-29 20:43:19 +0300

    fix url'

--- server.js
+++ server.js
@@ -8,15 +8,16 @@ app.use(express.urlencoded({ extended: true }));
 
 // Endpoint to handle form data
 app.post('/handle-form', (req, res) => {
+
   const { mac, ip, username, error } = req.body;
   const link_login = req.body['link-login'];
   const link_login_only = req.body['link-login-only'];
   const link_orig = req.body['link-orig'];
 
   const queryParams = new URLSearchParams({ mac, ip, username, link_login, link_login_only, link_orig, error }).toString();
-  console.log(`Server is calling handle-form with ${queryParams}`);
-
-  res.redirect(`/?${queryParams}`);
+  const url_string = `https://hotspot.kredoh.co.ke/?${queryParams}`
+  console.log(`Server is calling ${url_string}`);
+  res.redirect(url_string);
 });
 
 app.listen(PORT, () => {
