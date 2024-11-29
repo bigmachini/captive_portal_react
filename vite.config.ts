fd5baa1b87f22fb3ce261f4c7def51777b32ed08
bigmachini <elisha.wambiji@bigmachini.net>
2024-11-29 18:21:56 +0300

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  logLevel: 'error',
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://bigmachini.net',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
+});
